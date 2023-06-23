3c1fc65fcfeb87fc85c9f8651059a348266cc165
bigbiteci <ci@bigbite.net>
2023-06-23 13:52:02 +0000

    built from 97224c39acc24035e8b9040f002d9238bf9c2c93

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-plugins'), 'version' => 'd14d04c0912dc4a83eab');
--- src/editor/settings.js
+++
@@ -1,4 +0,0 @@
-export const NAMESPACE = 'big-bite-themer';
-export const STORE_NAME = 'big-bite/themer';
-export const FILTER_NAMESPACE = STORE_NAME;
-export const I18N_DOMAIN = 'themer';
