8bc1a5d793e47445d1b80a109910625fc55b8a3a
Ben Wells <ben@bigbite.net>
2023-07-14 17:10:38 +0100

    feat(api): checks if a valid theme.json is loaded

--- inc/class-rest-api.php
+++ inc/class-rest-api.php
@@ -42,7 +42,13 @@ class Rest_API {
 	 * @return \WP_REST_Response|\WP_Error theme.json generated stylesheet response data or WP_Error on failure.
 	 */
 	public function get_styles( \WP_REST_Request $request ) {
+		if ( true !== $theme_json_loaded ) {
+			$test = 'here';
+		}
+
 		$existing_theme_json = \WP_Theme_JSON_Resolver::get_merged_data();
+
+		$theme_json_loaded = self::can_load_theme_json();
 
 		if ( ! $existing_theme_json instanceof \WP_Theme_JSON ) {
 			return new \WP_Error( 'no_theme_json', __( 'Unable to locate existing theme.json data', 'themer' ) );
@@ -53,6 +59,21 @@ class Rest_API {
 		$custom_theme_json      = new \WP_Theme_JSON( $custom_theme_json_data );
 
 		return rest_ensure_response( $custom_theme_json->get_stylesheet() );
+	}
+
+	public function can_load_theme_json() {
+		$theme_json_path = get_stylesheet_directory() . '/theme.json';
+
+		if ( ! is_readable( $theme_json_path ) ) {
+				return sprintf( 'Cannot read the file "theme.json" from %v', $theme_json_path );
+		}
+
+		$value = wp_json_file_decode( $theme_json_path );
+		if ( null === $value ) {
+			return 'The theme.json file contains invalid json.';
+		}
+
+		return true;
 	}
 
 }
