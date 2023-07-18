f26089830ef2f4669883e67b2837cbaca6d1264e
Ben Wells <ben@bigbite.net>
2023-07-18 11:46:23 +0100

    doc(notice): add jsdoc

--- src/editor/components/ThemerNotice.js
+++ src/editor/components/ThemerNotice.js
@@ -1,5 +1,13 @@
 import { Notice } from '@wordpress/components';
 
+/**
+ * Component for displaying notices
+ *
+ * @param {Object} props
+ * @param {string} props.status
+ * @param {string} props.message
+ * @returns {false | React.JSX.Element}
+ */
 const ThemerNotice = ( { status, message = '' } ) => {
 	return (
 		message.length > 0 && (
