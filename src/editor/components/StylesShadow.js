e7e5d4462a2816dceb6f55dff677dedc40e5678f
Joe Rouse <45216035+Joe-Rouse@users.noreply.github.com>
2023-09-06 16:12:56 +0100

    feat(bbt-92): add toggle for inset

--- src/editor/components/StylesShadow.js
+++ src/editor/components/StylesShadow.js
@@ -1,7 +1,7 @@
 import { set } from 'lodash';
 import { __ } from '@wordpress/i18n';
 import { useContext } from '@wordpress/element';
-import { TextControl } from '@wordpress/components';
+import { TextControl, ToggleControl } from '@wordpress/components';
 
 import getThemeOption from '../../utils/get-theme-option';
 import EditorContext from '../context/EditorContext';
@@ -18,6 +18,23 @@ const Shadow = ({ selector }) => {
 	const { setUserConfig } = useContext(StylesContext);
 	const shadowStyles = getThemeOption(selector, themeConfig);
 
+	/**
+	 * Handles the user clicking the inset toggle.
+	 * @param {boolean} newVal - The new value of the inset toggle.
+	 */
+	const handleNewInsetValue = (newVal) => {
+		const updatedShadowStyles = newVal
+			? `inset ${shadowStyles}`
+			: shadowStyles.replace('inset', '').trim();
+		let config = structuredClone(themeConfig);
+		config = set(config, selector, updatedShadowStyles);
+		setUserConfig(config);
+	};
+
+	/**
+	 * Handles the user changing the shadow value.
+	 * @param {string} newValue - The new value of the shadow text field.
+	 */
 	const handleNewValue = (newValue) => {
 		let config = structuredClone(themeConfig);
 		config = set(config, selector, newValue);
@@ -30,6 +47,11 @@ const Shadow = ({ selector }) => {
 				{__('Shadow', 'themer')}
 			</span>
 			<div className="themer--blocks-item-component--columns themer--blocks-item-component--columns-2">
+				<ToggleControl
+					checked={shadowStyles.includes('inset')}
+					label={__('Inset', 'themer')}
+					onChange={(newVal) => handleNewInsetValue(newVal)}
+				/>
 				<TextControl
 					onChange={(newVal) => handleNewValue(newVal)}
 					value={shadowStyles}
