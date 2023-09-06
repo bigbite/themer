8002872ceee3040f85dbb8a0afd2d84432a736de
Joe Rouse <45216035+Joe-Rouse@users.noreply.github.com>
2023-09-06 15:47:41 +0100

    feat(bbt-92): basic shadow control with text input testing to see how the value saves to the DB

--- src/editor/components/StylesShadow.js
+++ src/editor/components/StylesShadow.js
@@ -1,8 +1,42 @@
+import { set } from 'lodash';
+import { __ } from '@wordpress/i18n';
+import { useContext } from '@wordpress/element';
+import { TextControl } from '@wordpress/components';
+
+import getThemeOption from '../../utils/get-theme-option';
+import EditorContext from '../context/EditorContext';
+import StylesContext from '../context/StylesContext';
+
 /**
  * Reusable shadow control style component
+ *
+ * @param {Object} props          Component props
+ * @param {string} props.selector Property target selector
  */
-const Shadow = () => {
-	return <h3>Shadow Component</h3>;
+const Shadow = ({ selector }) => {
+	const { themeConfig } = useContext(EditorContext);
+	const { setUserConfig } = useContext(StylesContext);
+	const shadowStyles = getThemeOption(selector, themeConfig);
+
+	const handleNewValue = (newValue) => {
+		let config = structuredClone(themeConfig);
+		config = set(config, selector, newValue);
+		setUserConfig(config);
+	};
+
+	return (
+		<>
+			<span className="themer--blocks-item-component--styles--title">
+				{__('Shadow', 'themer')}
+			</span>
+			<div className="themer--blocks-item-component--columns themer--blocks-item-component--columns-2">
+				<TextControl
+					onChange={(newVal) => handleNewValue(newVal)}
+					value={shadowStyles}
+				/>
+			</div>
+		</>
+	);
 };
 
 export default Shadow;
