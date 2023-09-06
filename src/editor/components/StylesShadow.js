3033470d0ffbb67647b4e7642aafc1d21e851329
Joe Rouse <45216035+Joe-Rouse@users.noreply.github.com>
2023-09-06 18:22:41 +0100

    style(bbt-92): fix prettier errors

--- src/editor/components/StylesShadow.js
+++ src/editor/components/StylesShadow.js
@@ -20,96 +20,99 @@ import StylesContext from '../context/StylesContext';
  * @param {Object} props          Component props
  * @param {string} props.selector Property target selector
  */
-const Shadow = ({ selector }) => {
-	const { themeConfig } = useContext(EditorContext);
-	const { setUserConfig } = useContext(StylesContext);
-	const shadowStyles = getThemeOption(selector, themeConfig);
+const Shadow = ( { selector } ) => {
+	const { themeConfig } = useContext( EditorContext );
+	const { setUserConfig } = useContext( StylesContext );
+	const shadowStyles = getThemeOption( selector, themeConfig );
 	const themePalette = getThemeOption(
 		'settings.color.palette.theme',
 		themeConfig
 	);
 
 	const LENGTH_REG = /^[0-9]+[a-zA-Z%]+?$/;
-	const isCssUnit = (value) =>
-		(value === '0' || LENGTH_REG.test(value)) && value !== 'inset';
+	const isCssUnit = ( value ) =>
+		( value === '0' || LENGTH_REG.test( value ) ) && value !== 'inset';
 
-	const shadowStylesArray = shadowStyles.split(' ');
-	const shadowValues = shadowStylesArray.filter(isCssUnit).slice(0, 4);
+	const shadowStylesArray = shadowStyles.split( ' ' );
+	const shadowValues = shadowStylesArray.filter( isCssUnit ).slice( 0, 4 );
 	const shadowColor = shadowStylesArray.find(
-		(value) => !isCssUnit(value) && value !== 'inset'
+		( value ) => ! isCssUnit( value ) && value !== 'inset'
 	);
 	const shadowObj = {
-		inset: shadowStyles.includes('inset') ? 'inset' : '',
-		offsetX: shadowValues?.[0] || '0px',
-		offsetY: shadowValues?.[1] || '0px',
-		blurRadius: shadowValues?.[2] || '0px',
-		spreadRadius: shadowValues?.[3] || '0px',
+		inset: shadowStyles.includes( 'inset' ) ? 'inset' : '',
+		offsetX: shadowValues?.[ 0 ] || '0px',
+		offsetY: shadowValues?.[ 1 ] || '0px',
+		blurRadius: shadowValues?.[ 2 ] || '0px',
+		spreadRadius: shadowValues?.[ 3 ] || '0px',
 		color: shadowColor || '#000',
 	};
 
-	/**
-	 * Handles the user changing one of the values in the box shadow.
-	 * @param {boolean} newVal - The new value of the setting.
-	 * @param {string} key - The key of the value being changed.
-	 */
-	const handleNewValue = (newVal, key) => {
-		if (key === 'inset') {
-			shadowObj[key] = newVal ? 'inset' : '';
+	const handleNewValue = ( newVal, key ) => {
+		if ( key === 'inset' ) {
+			shadowObj[ key ] = newVal ? 'inset' : '';
 		} else {
-			shadowObj[key] = newVal;
+			shadowObj[ key ] = newVal;
 		}
-		const updatedShadowStyles = Object.values(shadowObj).join(' ').trim();
+		const updatedShadowStyles = Object.values( shadowObj )
+			.join( ' ' )
+			.trim();
 
-		let config = structuredClone(themeConfig);
-		config = set(config, selector, updatedShadowStyles);
-		setUserConfig(config);
+		let config = structuredClone( themeConfig );
+		config = set( config, selector, updatedShadowStyles );
+		setUserConfig( config );
 	};
 
 	return (
 		<>
 			<span className="themer--blocks-item-component--styles--title">
-				{__('Shadow', 'themer')}
+				{ __( 'Shadow', 'themer' ) }
 			</span>
 			<VStack>
 				<ToggleControl
-					checked={shadowStyles.includes('inset')}
-					label={__('Inset', 'themer')}
-					onChange={(newVal) => handleNewValue(newVal, 'inset')}
+					checked={ shadowStyles.includes( 'inset' ) }
+					label={ __( 'Inset', 'themer' ) }
+					onChange={ ( newVal ) => handleNewValue( newVal, 'inset' ) }
 				/>
 				<HStack>
 					<UnitControl
-						label={__('Offset X', 'themer')}
-						value={shadowValues?.[0] || '0px'}
-						onChange={(newVal) => handleNewValue(newVal, 'offsetX')}
+						label={ __( 'Offset X', 'themer' ) }
+						value={ shadowValues?.[ 0 ] || '0px' }
+						onChange={ ( newVal ) =>
+							handleNewValue( newVal, 'offsetX' )
+						}
 					/>
 					<UnitControl
-						label={__('Offset Y', 'themer')}
-						value={shadowValues?.[1] || '0px'}
-						onChange={(newVal) => handleNewValue(newVal, 'offsetY')}
+						label={ __( 'Offset Y', 'themer' ) }
+						value={ shadowValues?.[ 1 ] || '0px' }
+						onChange={ ( newVal ) =>
+							handleNewValue( newVal, 'offsetY' )
+						}
 					/>
 				</HStack>
 				<HStack>
 					<UnitControl
-						label={__('Blur radius', 'themer')}
-						value={shadowValues?.[2] || '0px'}
-						onChange={(newVal) =>
-							handleNewValue(newVal, 'blurRadius')
+						label={ __( 'Blur radius', 'themer' ) }
+						value={ shadowValues?.[ 2 ] || '0px' }
+						onChange={ ( newVal ) =>
+							handleNewValue( newVal, 'blurRadius' )
 						}
 					/>
 					<UnitControl
-						label={__('Spread radius', 'themer')}
-						value={shadowValues?.[3] || '0px'}
-						onChange={(newVal) =>
-							handleNewValue(newVal, 'spreadRadius')
+						label={ __( 'Spread radius', 'themer' ) }
+						value={ shadowValues?.[ 3 ] || '0px' }
+						onChange={ ( newVal ) =>
+							handleNewValue( newVal, 'spreadRadius' )
 						}
 					/>
 				</HStack>
 				<ColorPalette
-					label={__('Shadow Colour', 'themer')}
-					colors={themePalette}
-					onChange={(newVal) => handleNewValue(newVal, 'color')}
+					label={ __( 'Shadow Colour', 'themer' ) }
+					colors={ themePalette }
+					onChange={ ( newVal ) => handleNewValue( newVal, 'color' ) }
 					value={
-						shadowColor && isHex(shadowColor) ? shadowColor : '#000'
+						shadowColor && isHex( shadowColor )
+							? shadowColor
+							: '#000'
 					}
 				/>
 			</VStack>
