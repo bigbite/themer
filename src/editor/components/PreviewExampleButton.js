5e82570b36817a6e662318699e582cc7f91fa54f
Gareth Elwell <gareth@bigbite.net>
2023-12-05 12:42:40 +0000

    refactor: extract PreviewExampleButton to dedicated component

---
+++ src/editor/components/PreviewExampleButton.js
@@ -0,0 +1,60 @@
+import {
+	Button,
+	__experimentalUseNavigator as useNavigator,
+} from '@wordpress/components';
+import { useContext, useEffect } from '@wordpress/element';
+import { seen } from '@wordpress/icons';
+
+import EditorContext from '../context/EditorContext';
+
+import { getPreviewFromRouteParams } from '../../utils/blockPreviews';
+
+/**
+ * Button to handle updating the preview example blocks
+ *
+ */
+const PreviewExampleButton = () => {
+	const {
+		previewExampleIsActive,
+		setPreviewExampleIsActive,
+		setPreviewBlocks,
+		resetPreviewBlocks,
+	} = useContext( EditorContext );
+
+	const { params } = useNavigator();
+
+	useEffect( () => {
+		if ( previewExampleIsActive ) {
+			const preview = getPreviewFromRouteParams( params );
+			setPreviewBlocks( preview );
+		} else {
+			resetPreviewBlocks();
+		}
+	}, [
+		params,
+		previewExampleIsActive,
+		resetPreviewBlocks,
+		setPreviewBlocks,
+	] );
+
+	const handlePreviewExampleToggle = () => {
+		if ( previewExampleIsActive ) {
+			setPreviewExampleIsActive( false );
+			resetPreviewBlocks();
+			return;
+		}
+		setPreviewExampleIsActive( true );
+	};
+
+	return (
+		<Button
+			className="themer-styles-heading__right"
+			onClick={ handlePreviewExampleToggle }
+			icon={ seen }
+			isPressed={ previewExampleIsActive }
+			label="Toggle example"
+		/>
+	);
+};
+
+export default PreviewExampleButton;
--- src/editor/components/PreviewToolbar.js
+++ src/editor/components/PreviewToolbar.js
@@ -1,62 +1,14 @@
-import {
-	Button,
-	__experimentalUseNavigator as useNavigator,
-} from '@wordpress/components';
-import { useContext, useEffect } from '@wordpress/element';
-import { seen } from '@wordpress/icons';
-
-import EditorContext from '../context/EditorContext';
-
-import { getPreviewFromRouteParams } from '../../utils/blockPreviews';
-
+import PreviewExampleButton from './PreviewExampleButton';
 import ResponsiveButton from './ResponsiveButton';
 
 /**
- * button to handle responsive preview options
+ * Toolbar to display options above the preview
  *
  */
 const PreviewToolbar = () => {
-	const {
-		previewExampleIsActive,
-		setPreviewExampleIsActive,
-		setPreviewBlocks,
-		resetPreviewBlocks,
-	} = useContext( EditorContext );
-
-	const { params } = useNavigator();
-
-	useEffect( () => {
-		if ( previewExampleIsActive ) {
-			const preview = getPreviewFromRouteParams( params );
-			setPreviewBlocks( preview );
-		} else {
-			resetPreviewBlocks();
-		}
-	}, [
-		params,
-		previewExampleIsActive,
-		resetPreviewBlocks,
-		setPreviewBlocks,
-	] );
-
-	const handlePreviewExampleToggle = () => {
-		if ( previewExampleIsActive ) {
-			setPreviewExampleIsActive( false );
-			resetPreviewBlocks();
-			return;
-		}
-		setPreviewExampleIsActive( true );
-	};
-
 	return (
 		<div className="themer-preview__toolbar">
-			<Button
-				className="themer-styles-heading__right"
-				onClick={ handlePreviewExampleToggle }
-				icon={ seen }
-				isPressed={ previewExampleIsActive }
-				label="Toggle example"
-			/>
+			<PreviewExampleButton />
 			<ResponsiveButton />
 		</div>
 	);
