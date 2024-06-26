764665d8a02aa9136daca7f356351646cf779ba5
Chris Hall <c.hall1@live.com>
2024-06-26 10:56:07 +0100

    feat: split settings and styles in code view to get headings

--- src/editor/components/CodeView.js
+++ src/editor/components/CodeView.js
@@ -5,7 +5,14 @@
  * @param {Object} props.themeConfig Current theme configuration
  */
 const CodeView = ( { themeConfig } ) => {
-	return <pre>{ JSON.stringify( themeConfig, null, 2 ) }</pre>;
+	return (
+		<>
+			<span className="themer--styles__item__title">Settings</span>
+			<pre>{ JSON.stringify( themeConfig.settings, null, 2 ) }</pre>
+			<span className="themer--styles__item__title">Styles</span>
+			<pre>{ JSON.stringify( themeConfig.styles, null, 2 ) }</pre>
+		</>
+	);
 };
 
 export default CodeView;
