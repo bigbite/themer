3ce7c8540743ef25af62491190e15ec320d91f14
bigbiteci <ci@bigbite.net>
2024-07-23 11:35:30 +0000

    built from dd2dc750722dfc8fd2d424b990766043f49e66cc

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-data', 'wp-deprecated', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-preferences', 'wp-primitives'), 'version' => '3cb69eca8fda87d4e370');
