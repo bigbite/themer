e858a123065e5f96fe05d2c6b21cfdedd723e1c9
Chris Hall <c.hall1@live.com>
2024-09-02 08:12:11 +0100

    feat: translation on strings and useMemo instead of function

--- src/editor/components/ClearCustomisation.js
+++ src/editor/components/ClearCustomisation.js
@@ -2,22 +2,22 @@
  * Component for clearing customisation for individual style components
  */
 import { set, get } from 'lodash';
-
+import { __ } from '@wordpress/i18n';
 import { DropdownMenu, MenuItem, MenuGroup } from '@wordpress/components';
 import { menu, trash } from '@wordpress/icons';
-import { useContext } from '@wordpress/element';
+import { useContext, useMemo } from '@wordpress/element';
 import StylesContext from '../context/StylesContext';
 
 const ClearCustomisation = ( { selector, userConfig, themeConfigVal } ) => {
 	const { setUserConfig } = useContext( StylesContext );
 
 	// Check if the current value is different from the default value to determine if button should be enabled.
-	const flagCustomisation = () => {
+	const flagCustomisation = useMemo( () => {
 		const currentVal = get( userConfig, selector );
 		if ( currentVal !== themeConfigVal ) {
 			return true;
 		}
-	};
+	}, [ selector, userConfig, themeConfigVal ] );
 
 	// Sets userConfig for selector to themeConfig value.
 	const onClick = () => {
@@ -26,16 +26,19 @@ const ClearCustomisation = ( { selector, userConfig, themeConfigVal } ) => {
 		setUserConfig( config );
 	};
 	return (
-		<DropdownMenu icon={ menu } label="Select an option">
+		<DropdownMenu
+			icon={ menu }
+			label={ __( 'Select an option', 'themer' ) }
+		>
 			{ () => (
 				<>
 					<MenuGroup>
 						<MenuItem
 							icon={ trash }
 							onClick={ onClick }
-							disabled={ ! flagCustomisation() }
+							disabled={ ! flagCustomisation }
 						>
-							Remove customisation
+							{ __( 'Remove customisation', 'themer' ) }
 						</MenuItem>
 					</MenuGroup>
 				</>
