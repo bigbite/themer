36d33442baa7e19c85d6db7550d3abc4c21e1651
g-elwell <g@elwell.co>
2024-09-30 23:49:32 +0100

    chore: remove unused files

--- src/editor/components/CustomBlocks.js
+++
@@ -1,6 +0,0 @@
-/**
- * Custom Blocks tab menu component
- */
-const CustomBlock = () => <p>Custom Block Tab</p>;
-
-export default CustomBlock;
--- src/editor/components/Layout.js
+++
@@ -1,6 +0,0 @@
-/**
- * Layout tab menu component
- */
-const Layout = () => <p>Layout Tab</p>;
-
-export default Layout;
