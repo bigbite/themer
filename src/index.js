acafa736d38e5526e99600cfd4bc2de2e5ea33d7
Gareth Elwell <41474928+g-elwell@users.noreply.github.com>
2024-10-01 22:29:22 +0100

    chore: correct comment wording

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import Themer from './editor/components/Themer';
 import './editor/styles/index.scss';
 
 /**
- * Initialise the editor and render the revisions page react component.
- * This function is called within the PHP template for the revisions page.
+ * Initialise the editor and render the themer react component.
+ * This function is called within the PHP template for the themer page.
  *
  * @param {string} id       The ID of the root element to render the editor into
  * @param {Object} settings The editor settings
