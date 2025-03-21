6ace27ef4e8f79c5247a34f2e8d912ca01354cf3
Gareth Elwell <41474928+g-elwell@users.noreply.github.com>
2025-03-21 09:09:09 +0000

    feat: Add block template manager (#125)
    * feat: add initial template manager view
    
    * fix: eslint errors
    
    * chore: remove commented out code

---
+++ src/editor/components/ButtonTemplateManager.js
@@ -0,0 +1,26 @@
+import { __ } from '@wordpress/i18n';
+import { MenuItem } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+import TemplateManagerModal from './TemplateManagerModal';
+
+/**
+ * Renders the button to export theme.json
+ */
+const ButtonTemplateManager = () => {
+	const [ isModalOpen, setIsModalOpen ] = useState( false );
+
+	return (
+		<>
+			<MenuItem
+				role="menuitem"
+				onClick={ () => setIsModalOpen( true ) }
+				info={ __( 'Manage block templates.', 'themer' ) }
+			>
+				{ __( 'Block Templates', 'themer' ) }
+			</MenuItem>
+			<TemplateManagerModal isModalOpen={ isModalOpen } />
+		</>
+	);
+};
+
+export default ButtonTemplateManager;
---
+++ src/editor/components/TemplateManagerModal.js
@@ -0,0 +1,82 @@
+import { __ } from '@wordpress/i18n';
+import { Button, Modal, ExternalLink } from '@wordpress/components';
+import { useEntityRecords, store as coreStore } from '@wordpress/core-data';
+import { useDispatch } from '@wordpress/data';
+
+import saveTemplate from '../../utils/save-template';
+import { download, backup } from '@wordpress/icons';
+
+/**
+ * Renders the button to export theme.json
+ *
+ * @param {Object}   props
+ * @param {boolean}  props.isOpen    Whether the modal is open
+ * @param {Function} props.setIsOpen Function to set the modal open state
+ */
+const TemplateManagerModal = ( { isOpen, setIsOpen } ) => {
+	const { deleteEntityRecord } = useDispatch( coreStore );
+
+	const { records: templates } = useEntityRecords(
+		'postType',
+		'wp_template',
+		{
+			per_page: 100,
+		}
+	);
+
+	if ( ! isOpen ) {
+		return null;
+	}
+
+	return (
+		<Modal
+			focusOnMount
+			title={ __( 'Block Templates', 'themer' ) }
+			onRequestClose={ () => setIsOpen( false ) }
+		>
+			<table>
+				{ templates.map( ( template ) => {
+					const isCustomised =
+						template.has_theme_file && template.source === 'custom';
+					const editUrl = `site-editor.php?postId=${ template.id }&postType=wp_template&canvas=edit`;
+					return (
+						<tr key={ template.id }>
+							<td>
+								<ExternalLink href={ editUrl }>
+									{ template.title.raw }
+								</ExternalLink>
+							</td>
+							<td>
+								{ isCustomised && (
+									<Button
+										icon={ backup }
+										isDestructive
+										onClick={ () =>
+											deleteEntityRecord(
+												'postType',
+												'wp_template',
+												template.id
+											)
+										}
+									>
+										{ __( 'Reset', 'themer' ) }
+									</Button>
+								) }
+							</td>
+							<td>
+								<Button
+									icon={ download }
+									onClick={ () => saveTemplate( template ) }
+								>
+									{ __( 'Export', 'themer' ) }
+								</Button>
+							</td>
+						</tr>
+					);
+				} ) }
+			</table>
+		</Modal>
+	);
+};
+
+export default TemplateManagerModal;
