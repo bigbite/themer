6f3439821e622a60b6099d8273b2fe00e1f733c0
bigbiteci <ci@bigbite.net>
2025-03-28 16:44:35 +0000

    built from 32d42bbac3d38832208d785b7c0217eaa3fd46bc

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-block-library', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ac9642edc23da630ce8b');
