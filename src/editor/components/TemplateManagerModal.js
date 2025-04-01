a9200d9ce4006ede12127470af24f1759f6bb660
Gareth Elwell <41474928+g-elwell@users.noreply.github.com>
2025-04-01 21:15:04 +0100

    fix: export block templates as HTML files

--- src/editor/components/TemplateManagerModal.js
+++ src/editor/components/TemplateManagerModal.js
@@ -3,7 +3,7 @@ import { Button, Modal, ExternalLink } from '@wordpress/components';
 import { useEntityRecords, store as coreStore } from '@wordpress/core-data';
 import { useDispatch } from '@wordpress/data';
 
-import { savePhpFile } from '../../utils/save-file';
+import { saveHtmlFile } from '../../utils/save-file';
 import { download, backup } from '@wordpress/icons';
 
 /**
@@ -66,7 +66,7 @@ const TemplateManagerModal = ( { isOpen, setIsOpen } ) => {
 							<td>
 								<Button
 									icon={ download }
-									onClick={ () => savePhpFile( template ) }
+									onClick={ () => saveHtmlFile( template ) }
 								>
 									{ __( 'Export', 'themer' ) }
 								</Button>
--- src/utils/save-file.js
+++ src/utils/save-file.js
@@ -22,20 +22,17 @@ export const saveJsonFile = async ( filename, data ) => {
  * @param {Object} data block template
  */
 export const savePhpFile = async ( data ) => {
-	// Create PHP template header
-	let phpContent = '';
-	if ( 'wp_block' === data.type ) {
-		const name = data?.title?.raw || 'Untitled';
-		const description = data?.description?.raw || '';
-		const categories =
-			data.wp_pattern_category.length > 0
-				? data?._embedded[ 'wp:term' ][ 0 ]
-						.map( ( category ) => category.name )
-						.join( ', ' )
-				: '';
-		const slug = data?.slug || '';
+	const name = data?.title?.raw || 'Untitled';
+	const description = data?.description?.raw || '';
+	const categories =
+		data.wp_pattern_category.length > 0
+			? data?._embedded[ 'wp:term' ][ 0 ]
+					.map( ( category ) => category.name )
+					.join( ', ' )
+			: '';
+	const slug = data?.slug || '';
 
-		const patternContent = `<?php
+	const phpContent = `<?php
 /**
  * Template Name: ${ name }
  * Slug: ${ slug }
@@ -45,18 +42,6 @@ ${ categories && ` * Categories: ${ categories }` }
 ?>
 
 ${ data.content.raw }`;
-		phpContent = patternContent;
-	} else {
-		const templateContent = `<?php
-/**
- * Template Name: ${ data.title.raw }
- */
-
-?>
-
-${ data.content.raw }`;
-		phpContent = templateContent;
-	}
 
 	const blob = new Blob( [ phpContent ], {
 		type: 'application/x-httpd-php',
@@ -69,6 +54,35 @@ ${ data.content.raw }`;
 				description: 'PHP Files',
 				accept: {
 					'application/x-httpd-php': [ '.php' ],
+				},
+			},
+		],
+	} );
+	const stream = await handle.createWritable();
+
+	await stream.write( blob );
+	await stream.close();
+};
+
+/**
+ * Save JSON blob to a file as a PHP template or pattern depending on the type
+ *
+ * @param {Object} data block template
+ */
+export const saveHtmlFile = async ( data ) => {
+	const htmlContent = data.content.raw;
+
+	const blob = new Blob( [ htmlContent ], {
+		type: 'application/x-httpd-php',
+	} );
+
+	const handle = await window.showSaveFilePicker( {
+		suggestedName: `${ data.slug }.html`,
+		types: [
+			{
+				description: 'HTML Files',
+				accept: {
+					'application/x-httpd-html': [ '.html' ],
 				},
 			},
 		],
