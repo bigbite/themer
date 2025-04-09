3b0dfb0df1f37788b39e6d96762cde5b8c20e6f1
Gareth Elwell <41474928+g-elwell@users.noreply.github.com>
2025-04-09 16:25:16 +0100

    Add file check data (#136)
    * refactor: use controller pattern in rest api, fix permissions
    
    * refactor: update casing and spacing
    
    * chore: add missing file doc comment
    
    * feat: compare file modified time with database modified time and expose in REST API
    
    * feat: show indicator when template may have changes in the database
    
    * chore: remove unused variable
    
    * refactor: move rest field callback into class method
    
    * refactor: convert to arrow function
    
    * fix: i18n string
    
    * fix: check file exists before attempting to get modified time

--- src/editor/components/TemplatePartManagerModal.js
+++ src/editor/components/TemplatePartManagerModal.js
@@ -1,10 +1,16 @@
 import { __ } from '@wordpress/i18n';
-import { Button, Modal, ExternalLink } from '@wordpress/components';
+import {
+	Button,
+	Icon,
+	Modal,
+	Tooltip,
+	ExternalLink,
+} from '@wordpress/components';
 import { useEntityRecords, store as coreStore } from '@wordpress/core-data';
 import { useDispatch } from '@wordpress/data';
 
 import { saveHtmlFile } from '../../utils/save-file';
-import { download, backup } from '@wordpress/icons';
+import { download, backup, warning } from '@wordpress/icons';
 
 /**
  * Renders the button to export theme.json
@@ -39,11 +45,29 @@ const TemplatePartManagerModal = ( { isOpen, setIsOpen } ) => {
 					{ templateParts.map( ( templatePart ) => {
 						const isCustom = templatePart.source === 'custom';
 						const hasThemeFile = templatePart.has_theme_file;
+						const hasChanges = templatePart.themer_has_changes;
 
 						const editUrl = `site-editor.php?postId=${ templatePart.id }&postType=wp_template_part&canvas=edit`;
 
 						return (
 							<tr key={ templatePart.id }>
+								<td>
+									{ hasChanges && (
+										<Tooltip
+											text={ __(
+												'Database may have changes',
+												'themer'
+											) }
+										>
+											<Icon
+												icon={ warning }
+												style={ {
+													fill: 'var(--wp--preset--color--luminous-vivid-amber)',
+												} }
+											/>
+										</Tooltip>
+									) }
+								</td>
 								<td>
 									<ExternalLink href={ editUrl }>
 										{ templatePart.title.raw }
